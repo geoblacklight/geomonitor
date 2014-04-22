386033e5d26bc94b1adccc631ef3bfe52bd0ef85
Jack Reed <pjreed@stanford.edu>
2014-04-22 10:16:50 -0700

    doesnt crash on infinity calculation, but still needs work

--- lib/geomonitor/tools.rb
+++ lib/geomonitor/tools.rb
@@ -66,8 +66,21 @@ module Geomonitor::Tools
     lat_rad = lat_deg/180 * Math::PI
     n = 2.0 ** zoom
     x = ((lng_deg + 180.0) / 360.0 * n).to_i
-    y = ((1.0 - Math::log(Math::tan(lat_rad) + (1 / Math::cos(lat_rad))) / Math::PI) / 2.0 * n).to_i
+    y = ((1.0 - Math::log(Math::tan(lat_rad) + (1 / Math::cos(lat_rad))) / Math::PI) / 2.0 * n)
+    if is_infinity?(y)
+      y = x
+    else
+      y = y.to_i
+    end
     {:x => x, :y =>y}
+  end
+
+  def self.is_infinity?(num)
+    if num.infinite?.nil?
+      false
+    else
+      true
+    end
   end
 
   # Get the lat/lng for a specific tile at a zoom level
