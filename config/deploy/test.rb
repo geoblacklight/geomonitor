b3044b3b30b758ca390120f654cf1ec7e7a328b8
Jack Reed <pjreed@stanford.edu>
2014-09-09 13:41:20 -0700

    change test to dev

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -4,4 +4,4 @@ set :bundle_without, %w{production test}.join(' ')
 server fetch(:deploy_host), user: fetch(:user), roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, 'production'
+set :rails_env, 'development'
