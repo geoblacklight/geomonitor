0a16219666447859db76f0e6085ab07956c92379
Jessie Keck <jessie.keck@gmail.com>
2017-07-17 14:45:31 -0700

    Add the user name and host for the deploy environments.

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,7 +1,6 @@
-set :deploy_host, ask('Server', 'e.g. server.stanford.edu')
 set :bundle_without, %w{production test}.join(' ')
 
-server fetch(:deploy_host), user: fetch(:user), roles: %w{web db app}
+server 'kurma-monitor-dev.stanford.edu', user: fetch(:user), roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'development'
--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,7 +1,6 @@
-set :deploy_host, ask('Server', 'e.g. server.stanford.edu')
 set :bundle_without, %w{development test}.join(' ')
 
-server fetch(:deploy_host), user: fetch(:user), roles: %w{web db app}
+server 'kurma-monitor-stage.stanford.edu', user: fetch(:user), roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
